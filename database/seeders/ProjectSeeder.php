701520d538b66178de7a166c4d37ed711e8bf5fc
xJustFox <dodydaversa@gmail.com>
2024-02-21 16:58:06 +0100

    [Upgrade] Table populated

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use Faker\Factory as Faker;
 
 use App\Models\Project;
@@ -17,6 +18,21 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        // 
+        $faker = Faker::create();
+        
+        for ($i=0; $i < 10; $i++) {
+            $name = $faker->sentence(3);
+            $slug = Str::slug($name);
+            
+            Project::create([
+                'name' => $name,
+                'slug' => $slug,
+                'repository_link' => $faker->url,
+                'description' => $faker->paragraph,
+                'date_start' => $faker->date,
+                'date_end' => $faker->optional()->date,
+                'img' => $faker->imageUrl(),
+            ]);
+        }
     }
 }
