c239cb9c90674b7082b77fc87488accc41bb5782
xJustFox <dodydaversa@gmail.com>
2024-02-22 17:34:44 +0100

    [FIx] Fixed Edit Form

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -11,7 +11,7 @@
                 {{-- Name Proj --}}
                 <div class="col-6 mt-3 ">
                     <label class="form-label my-label" for="nameProject">Name Project</label>
-                    <input class="form-control form-control-sm @error('name') is-invalid border-danger @enderror" type="text" name="name" id="nameProject" aria-describedby="nameError" required>
+                    <input class="form-control form-control-sm @error('name') is-invalid border-danger @enderror" type="text" name="name" id="nameProject" aria-describedby="nameError" required value="{{$project['name']}}">
                     @error('name')
                         <div id="nameError" class="form-text text-danger">{{$message}}</div>
                     @enderror
@@ -20,7 +20,7 @@
                 {{-- Link Proj --}}
                 <div class="col-6 mt-3 ">
                     <label class="form-label my-label" for="linkProject">Link Project</label>
-                    <input class="form-control form-control-sm @error('repository_link') is-invalid border-danger @enderror" type="text" name="repository_link" id="linkProject" aria-describedby="linkProjectError" required>
+                    <input class="form-control form-control-sm @error('repository_link') is-invalid border-danger @enderror" type="text" name="repository_link" id="linkProject" aria-describedby="linkProjectError" required value="{{$project['repository_link']}}">
                     @error('repository_link')
                         <div id="linkProjectError" class="form-text text-danger">{{$message}}</div>
                     @enderror
@@ -29,13 +29,13 @@
                 {{-- Img Proj --}}
                 <div class="col-12 mt-3 ">
                     <label class="form-label my-label" for="imgProject">Project Image</label>
-                    <input class="form-control form-control-sm" type="text" name="img" id="imgProject">
+                    <input class="form-control form-control-sm" type="text" name="img" id="imgProject" value="{{$project['img']}}">
                 </div>
     
                 {{-- Date Start Proj --}}
                 <div class="col-6 mt-3 ">
                     <label class="form-label my-label" for="startDateProject">Start Date Project</label>
-                    <input class="form-control form-control-sm @error('date_start') is-invalid border-danger @enderror" type="date" name="date_start" id="startDateProject" aria-describedby="startDateError" required>
+                    <input class="form-control form-control-sm @error('date_start') is-invalid border-danger @enderror" type="date" name="date_start" id="startDateProject" aria-describedby="startDateError" required value="{{$project['date_start']}}">
                     @error('date_start')
                         <div id="dateStartError" class="form-text text-danger">{{$message}}</div>
                     @enderror
@@ -44,7 +44,7 @@
                 {{-- Date End Proj --}}
                 <div class="col-6 mt-3 ">
                     <label class="form-label my-label" for="endDateProject">End Date Project</label>
-                    <input class="form-control form-control-sm @error('date_end') is-invalid border-danger @enderror" type="date" name="date_end" id="endDateProject" aria-describedby="endDateError">
+                    <input class="form-control form-control-sm @error('date_end') is-invalid border-danger @enderror" type="date" name="date_end" id="endDateProject" aria-describedby="endDateError" value="{{$project['date_end']}}">
                     @error('date_end')
                         <div id="dateEndError" class="form-text text-danger">{{$message}}</div>
                     @enderror
@@ -52,7 +52,7 @@
     
                 <div class="col-12 mt-3 ">
                     <label class="form-label my-label" for="descriptionProject">Project Description</label>
-                    <textarea class="form-control form-control-sm @error('description') is-invalid border-danger @enderror" name="description" id="descriptionProject" cols="30" rows="10" aria-describedby="descriptionError" required></textarea>
+                    <textarea class="form-control form-control-sm @error('description') is-invalid border-danger @enderror" name="description" id="descriptionProject" cols="30" rows="10" aria-describedby="descriptionError" required>{{$project['description']}}</textarea>
                     @error('description')
                         <div id="descriptionError" class="form-text text-danger">{{$message}}</div>
                     @enderror
