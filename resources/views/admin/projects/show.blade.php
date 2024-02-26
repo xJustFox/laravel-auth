3360a158803a4e2b891541129cafc32c67c1877b
xJustFox <dodydaversa@gmail.com>
2024-02-26 13:15:34 +0100

    [Fix] show img

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <div class="container-lg mt-3 detailesPrj">
         @if ($project['img'] != null)
+            @if (Str::contains($project->img, 'https'))
+            <img class="float-end mx-2" src="{{ $project->img }}" alt="">
+            @else
             <img class="float-end mx-2" src="{{asset('/storage/' . $project->img)}}" alt="">
+            @endif
         @else
             <img class="float-end mx-2" src="https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg" alt="">
         @endif
